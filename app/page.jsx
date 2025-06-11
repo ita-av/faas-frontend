c69342c9d0853b490831d99efcf7c9ad123afac3
Andraz Valentincic <andrazvalentincic@yahoo.com>
2025-06-11 18:10:43 +0200

    render dashboard when signed in

--- app/page.jsx
+++ app/page.jsx
@@ -1,23 +1,12 @@
 "use client";
 
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
-import { onAuthStateChanged } from "firebase/auth";
-import { auth } from "./firebase";
+import { ProtectedRoute } from "./components/RouteGuards";
+import Dashboard from "./components/Dashboard";
 
 export default function Home() {
-  const router = useRouter();
-  useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        router.push("/dashboard");
-      } else {
-        router.push("/login");
-      }
-    });
-
-    return () => unsubscribe();
-  }, [router]);
-
-  return null; // or <p>Loading...</p> if you want
+  return (
+    <ProtectedRoute>
+      <Dashboard />
+    </ProtectedRoute>
+  );
 }
